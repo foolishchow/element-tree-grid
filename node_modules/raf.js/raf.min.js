45799c4673cdbc8ef071bf25d3c1ed5739180a76
Gogs <gogs@fake.local>
2017-04-20 12:52:10 +0800

    add remote props

--- node_modules/raf.js/raf.min.js
+++
@@ -1,1 +0,0 @@
-!function(n){for(var e=0,a=["webkit","moz"],t=n.requestAnimationFrame,i=n.cancelAnimationFrame,m=a.length;--m>=0&&!t;)t=n[a[m]+"RequestAnimationFrame"],i=n[a[m]+"CancelAnimationFrame"];t&&i||(t=function(n){var a=+new Date,t=Math.max(e+16,a);return setTimeout(function(){n(e=t)},t-a)},i=clearTimeout),n.requestAnimationFrame=t,n.cancelAnimationFrame=i}(window);
--- node_modules/uppercamelcase/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-var camelCase = require('camelcase');
-
-module.exports = function () {
-	var cased = camelCase.apply(camelCase, arguments);
-	return cased.charAt(0).toUpperCase() + cased.slice(1);
-};
